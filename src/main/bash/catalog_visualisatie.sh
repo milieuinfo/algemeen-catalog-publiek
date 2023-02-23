f01ed3d7da096cb370c61d05ed9d08f3b12f574c
gezever <gezever@skynet.be>
2023-02-23 16:43:58 +0100

    versies in packages

--- src/main/bash/catalog_visualisatie.sh
+++ src/main/bash/catalog_visualisatie.sh
@@ -2,10 +2,12 @@
 
 sparql --data=../resources/be/vlaanderen/omgeving/data/id/catalog/algemeen-catalog-publiek/catalog.ttl --query ../sparql/catalog_visualisatie.sparql --results=TURTLE | rdf2dot  | dot -Tpng > ../sparql/catalog_visualisatie.png
 pushd  ../../..
+git pull
 mvn clean install
-unzip target/algemeen-catalog-publiek-1.1.0-SNAPSHOT.jar -d target
+jar=`ls target/*.jar | grep -v sources | grep -v original`
+unzip $jar -d target
 find target/be  | grep ttl | xargs riot --formatted=TURTLE > /tmp/catalog.ttl
 popd
-sparql --data=/tmp/catalog.ttl --query ../sparql/catalog_visualisatie.sparql --results=TURTLE | rdf2dot  | dot -Tpng > ../sparql/volledige_catalog_visualisatie.png
+sparql --data=/tmp/catalog.ttl --query ../sparql/catalog_visualisatie.sparql --results=TURTLE | rdf2dot  | sed -e 's/digraph {/digraph {rankdir="LR"/' | dot -Tpng > ../sparql/volledige_catalog_visualisatie.png
 
 
